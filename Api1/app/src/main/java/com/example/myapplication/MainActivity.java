4e9e90e2d945b3849233fea5605dfbe8f6318811
htlnutzer <32506360+htlnutzer@users.noreply.github.com>
2019-02-22 15:26:26 +0100

    BottomNav

---
+++ Api1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new ProductFragment());
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch(item.getItemId()){
+            case R.id.navigation_products:
+                fragment = new ProductFragment();
+                break;
+
+            case R.id.navigation_stats:
+                fragment = new StatsFragment();
+                break;
+
+            case R.id.navigation_tipps:
+                fragment = new TippsFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+}
