fe0f3a3b7cf31c6d157c62e2ce1b48a7b0af0d0c
htlnutzer <32506360+htlnutzer@users.noreply.github.com>
2019-02-24 20:14:45 +0100

    update

--- Api1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Api1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private ImageButton btn1;
+    private ImageButton btn2;
+    private ImageButton btn3;
+    private ImageButton btn4;
+    private ImageButton btn5;
+    private ImageButton btn6;
+    private TextView textView;
 
+    private int flaschenProTag;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String BTN1 = "btn1";
+
+    private String text;
 
 
     @Override
@@ -21,9 +39,83 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
-        loadFragment(new ProductFragment());
+
+
+        btn1 = (ImageButton) findViewById(R.id.btn1);
+        btn2 = (ImageButton) findViewById(R.id.btn2);
+        btn3 = (ImageButton) findViewById(R.id.btn3);
+        btn4 = (ImageButton) findViewById(R.id.btn4);
+        btn5 = (ImageButton) findViewById(R.id.btn5);
+        btn6 = (ImageButton) findViewById(R.id.btn6);
+        textView = (TextView) findViewById(R.id.txt1);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flaschenProTag = flaschenProTag+15;
+                textView.setText(flaschenProTag);
+            }
+        });
+
+       btn2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
+        public void saveData(){
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString(TEXT, textView.getText().toString());
+
+            editor.apply();
+
+            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+        }
+
+        public void loadData(){
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            text = sharedPreferences.getString(TEXT, "");
+        }
+
+        public void updateViews(){
+            textView.setText(text);
+        }
+
+
 
     private boolean loadFragment(Fragment fragment){
         if(fragment != null){
--- Api1/app/src/main/java/com/example/myapplication/ProductFragment.java
+++ Api1/app/src/main/java/com/example/myapplication/ProductFragment.java
@@ -1,21 +1,31 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ProductFragment extends Fragment {
 
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_product, null);
+
+
+
     }
 }
