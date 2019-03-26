fd8631af4f5f606250bb59fe5c3dab1135442308
htlnutzer <32506360+htlnutzer@users.noreply.github.com>
2019-03-26 11:58:07 +0100

    Update

--- Projekt-Plastic-fc828995b9d1a72e82aa3c283347fe5c1e7a4c49/Api1/app/src/main/java/com/example/myapplication/TippsFragment.java
+++ Projekt-Plastic-fc828995b9d1a72e82aa3c283347fe5c1e7a4c49/Api1/app/src/main/java/com/example/myapplication/TippsFragment.java
@@ -32,14 +32,31 @@ public class TippsFragment extends Fragment {
 
         switch(mostUsedProduct){
             case "Flasche":
-                tv3.setText("Try to take a glass Bottle with you");
+                tv3.setText("Try to take a glass bottle with you. It is much better for the environment. If you are not able to take" +
+                        " a glass bottle with you it is much better to a beverage that is " +
+                        "stored in a glass bottle");
+                break;
             case "Einkaufsack":
-                tv3.setText("Try to buy a stoff bag and take it with you");
+                tv3.setText("Nowadays you can get a fabric bag basically everywhere. It is also much more convenient to store one." +
+                        " There are even fabric bags that can be stored in a wallet.  ");
+                break;
+            case "Tupperware":
+                tv3.setText("Instead of buying tupperware to store food ore beverages, try to buy glass containers. They are " +
+                        "available everywhere and are much better for the environment. They are also cheaper than plastic containers ");
+                break;
+            case "Folie":
+                tv3.setText("When it comes to foil it is difficult to find something that can hold up to it" +
+                        " but what you can try is reusing the foil so you don't have to take one every single day");
+                break;
+            case "Kaffee":
+                tv3.setText("Most of the machines offer a carton cup instead of a plastic cup. This carton often times is more expensive" +
+                        "but it is worth the money ");
+                break;
+            case "Chips":
+                tv3.setText("Many of us love to eat crisps but they are not healthy for you and even unhealthier for the environment." +
+                        "Try to step down your chips game to half of what you usually eat and you will clearly see the results");
+                break;
         }
-
-       // if(db.getMostUsedProduct().equals("bag")) {
-       //     tv3.setText("Try bying a stoff Sack it is cheeper and you can take it with you all the time" );
-       // }
 
         return v;
     }
